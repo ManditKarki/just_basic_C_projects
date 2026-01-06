06ebed226737b6cec1eedffb8a552cafcbf8a7e1
ManditKarki <mandit.karki@gmail.com>
2026-01-06 21:15:50 +0545

    student data base

---
+++ Index.c
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+struct studentdata{
+    char name[50];
+    int age;
+    float marks;
+    int id;
+    char phone[20];
+    char email[50];
+};
